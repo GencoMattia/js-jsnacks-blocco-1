03590b0d00b47df884c071f4a890e9989a92802f
GencoMattia <gencomattia@hotmail.it>
2024-05-13 11:50:32 +0200

    snack 1

---
+++ Snack-1/js/script.js
@@ -0,0 +1,11 @@
+// L'utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+firstUserNumber = prompt ("Inserisci il primo numero")
+secondUserNumber = prompt ("Inserisci il secondo numero")
+
+if (firstUserNumber > secondUserNumber) {
+    console.log (firstUserNumber + " è maggiore")
+} else {
+    console.log (secondUserNumber + " è maggiore")
+}
