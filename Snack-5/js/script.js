cec387266e9f80346af089a0805e189a861be2b6
GencoMattia <gencomattia@hotmail.it>
2024-05-13 12:40:35 +0200

    added ;

--- Snack-5/js/script.js
+++ Snack-5/js/script.js
@@ -2,15 +2,15 @@
 //  Chiedi per 6 volte all'utente di inserire un numero,
 //  se è dispari inseriscilo nell'array.
 
-const array = []
+const array = [];
 
-let userNumber = ""
+let userNumber = "";
 
 for (let = i = 0; i < 6; i++) {
-    userNumber = Number.parseInt (prompt ("Scegli un numero"), 10)
+    userNumber = Number.parseInt (prompt ("Scegli un numero"), 10);
 
     if (userNumber % 2 === 1) {
-        array.push (userNumber)
+        array.push (userNumber);
     } else {
     
     }
