d3b7b3985821c0b99f0c35bac8db0de831986d93
GencoMattia <gencomattia@hotmail.it>
2024-05-13 12:24:46 +0200

    corretto snack-4

--- Snack-4/script.js
+++ Snack-4/script.js
@@ -1,15 +1,21 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
 
-const partyAttendantsList = ["Giangiorgio", "Pierluigi", "Annamaria", "Benedettamarcella", "Marialuigia"]
+const partyAttendantsList = ["Giangiorgio", "Pierluigi", "Annamaria", "Benedettamarcella", "Marialuigia"];
 
-const userName = prompt ("Inserisci il tuo nome")
+const userName = prompt ("Inserisci il tuo nome");
+userName = userName.charAt(0).toUpperCase() + userName.substring(1, userName.length).toLowerCase();
 
-for (index = 0; index < partyAttendantsList.length; index++) {
-    console.log (index, partyAttendantsList[index])
-    if (userName === partyAttendantsList[index]) {
-        console.log ("Congratulazioni, puoi entrare")
-    } else {
-        console.log ("Sparisci straccione/a")
-    }
-
+if (partyAttendantsList.includes(userName) === true) {
+    console.log ("Congratulazioni, puoi entrare");
+} else {
+    console.log ("Sparisci straccione/a");
 }
+
+// for (index = 0; index < partyAttendantsList.length; index++) {
+//     // console.log (index, partyAttendantsList[index]);
+//     if (userName === partyAttendantsList[index]) {
+//         console.log ("Congratulazioni, puoi entrare");
+//     } else {
+//         console.log ("Sparisci straccione/a");
+//     }
+// }
