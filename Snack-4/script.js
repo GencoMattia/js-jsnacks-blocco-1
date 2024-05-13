71ca2f8724f4c818760a06690b8ca41d1a5c6356
GencoMattia <gencomattia@hotmail.it>
2024-05-13 12:10:36 +0200

    added cicle and if inside the cicle

--- Snack-4/script.js
+++ Snack-4/script.js
@@ -1,3 +1,15 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 const partyAttendantsList = ["Giangiorgio", "Pierluigi", "Annamaria", "Benedettamarcella", "Marialuigia"]
+
+const userName = prompt ("Inserisci il tuo nome")
+
+for (index = 0; index < partyAttendantsList.length; index++) {
+    console.log (index, partyAttendantsList[index])
+    if (userName === partyAttendantsList[index]) {
+        console.log ("Congratulazioni, puoi entrare")
+    } else {
+        console.log ("Sparisci straccione/a")
+    }
+
+}
