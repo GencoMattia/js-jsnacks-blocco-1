44e63ac23b9bfdb20ac17ca012e24b29f35ed1a6
GencoMattia <gencomattia@hotmail.it>
2024-05-14 11:48:48 +0200

    snack 8 testing with creating divs

--- Snack-8/js/script.js
+++ Snack-8/js/script.js
@@ -1,8 +1,10 @@
 // Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
 
-document.createElement ("div");
+let textRed = document.createElement ("div");
 
-let body = document.querySelector ("body");
+let container = document.querySelector ("div.container");
+console.log (container);
+
 
 let numbers = [];
 
