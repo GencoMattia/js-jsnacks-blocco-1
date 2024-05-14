5574da5a19ca9762ef8c05130583100da9169682
GencoMattia <gencomattia@hotmail.it>
2024-05-14 10:54:13 +0200

    snack-6

---
+++ Snack-6/js/script.js
@@ -0,0 +1,26 @@
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+let array = [];
+let somma = 0;
+
+// for (let i = 1; i <= 50; i++) {
+//     array.push (Number.parseInt(i), 10);
+//     unevenNumbers.push (Number.parseInt (i % 2 === 1), 10)
+
+//     console.log (array)
+//     console.log (unevenNumbers)
+// }
+
+for (let i = 1; i <= 50; i++) {
+    array.push (i);
+
+    console.log (array)
+}
+
+for ( let index = 0; index < array.length; index++) {
+    if (index % 2 === 1) {
+        somma += array [index]
+    }
+}
+
+console.log (array, somma);
